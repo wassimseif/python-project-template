02f30e28c3e74150af9f9097ab67f1e815bbcbe3
Wassim Seifeddine <wassim_seifeddine95@live.com>
2023-11-07 11:07:26 +0100

    Small

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/Project.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/Project.py
@@ -12,7 +12,7 @@ class Project:
     module_dir: Path = Path(__file__).parent
     project_dir: Path = Path(__file__).parents[1]
 
-    config_dir = module_dir / "config"
+    config_dir = project_dir / "config"
     main_config_file = config_dir / "config.yml"
 
     data_dir = project_dir / "data"
@@ -21,7 +21,6 @@ class Project:
     export_dir = project_dir / "exports"
 
     def __post_init__(self) -> None:
-
         # create the directories if they don't exist
         self.data_dir.mkdir(exist_ok=True)
         self.log_dir.mkdir(exist_ok=True)
